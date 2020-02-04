07dbd1b9c5d5c96b7e21bd3e5646ea4b6cfdf33d
AnaKarolina52 <karol.teatro@me.com>
2020-02-04 15:22:29 +0000

    ‘commit’

---
+++ lab.js
@@ -0,0 +1,59 @@
+var myAlphabet;
+myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log(myAlphabet);
+function myAlphabetLength(){
+console.log(myAlphabet. length);
+}
+
+var planet;
+planet = ['terra','marte','mercurio','neturno','venus'];
+console.log(planet);
+console.log(planet[0]);
+console.log(planet[1]);
+console.log(planet[2]);
+console.log(planet[3]);
+console.log(planet[4]);
+
+var wowDatatypes;
+wowDatatypes = ['ana','karol','dias'];
+console.log(wowDatatypes);
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
+
+var x = 5;
+var y = 10;
+var z = 15;
+
+var xx = x+y;
+var xy = y-x;
+var x_y = x*y;
+var xxy = x/y;
+var xxx = x%2;
+var xplus = x++;
+var xminus = x--;
+
+console.log(xx);
+console.log(xy);
+console.log(x_y);
+console.log(xxy);
+console.log(xxx);
+console.log(xplus);
+console.log(xminus);
+
+var a = 5;
+var b = "Heloo";
+var c = " World!";
+var d = 10;
+
+console.log(a+b);
+console.log(b+c);
+console.log(a+d);
+
+// question 10
+var a = 5; 
+var b = 10;
+var c = "5"; 
+var d = 5 - a;
+
+console.log(a == c);
